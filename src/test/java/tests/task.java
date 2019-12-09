7f71c5ba3d217a2474c37b5399ec4fdcf68c17ec
burakbudakuk <57195566+burakbudakuk@users.noreply.github.com>
2019-12-09 18:18:03 +0000

    Update task.java

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -4,9 +4,11 @@ public class task {
   
   public static void main(String[] args){
    
-    System.out.printle("Hello world from github");
-    
-    
+    System.out.printle("Hello world from github"); 
+  }
+  
+  public String hello(){
+ return "Hello from github";
   }
   
 }
